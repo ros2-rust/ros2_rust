1581a61f1376498b66a05d70e0202a2a19f55d48
Jacob Hassold <jhassold@dcscorp.com>
2022-02-25 18:49:29 +0000

    Simplified project inclusion algorithm Distro A, OPSEC #4584. You may have additional rights; please see https://rosmilitary.org/faq/?category=ros-2-license
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- rclrs/build.rs
+++ rclrs/build.rs
@@ -1,41 +1,7 @@
 extern crate bindgen;
 
 use std::env;
-use std::path::{Path, PathBuf};
-
-fn generate_clang_include_arg(ament_prefix_path: &str) -> Option<Vec<String>> {
-    let ament_prefix_path = Path::new(ament_prefix_path);
-    if ament_prefix_path.exists() && ament_prefix_path.is_dir() {
-        let include_dir = ament_prefix_path.join(Path::new("include"));
-        if include_dir.exists() {
-            // If the path exists, there should be a directory name. Otherwise, abort
-            let project_name = ament_prefix_path.file_name().unwrap();
-
-            // Always include top-level include dir
-            let mut clang_flags = Vec::<String>::new();
-            clang_flags.push(format!("-I{}", include_dir.to_str().unwrap()));
-            
-            // Generate potential include path
-            let nested_project_include_path = include_dir.join(Path::new(project_name));
-            println!(
-                "\nNested project include path: {}",
-                nested_project_include_path.to_str().unwrap()
-            );
-
-            if nested_project_include_path.exists() {
-                println!("Nested path exists!");
-                // The path was created from only `str` objects, this should be safe to do
-                let generated_arg = format!("-I{}", nested_project_include_path.to_str().unwrap());
-                println!("Generated clang_arg: {}\n", generated_arg);
-                clang_flags.push(generated_arg)
-            } 
-
-            return Some(clang_flags);
-        }
-    }
-
-    None
-}
+use std::path::PathBuf;
 
 fn main() {
     let mut builder = bindgen::Builder::default()
@@ -63,9 +29,26 @@ fn main() {
 
     if let Ok(ament_prefix_paths) = ament_prefix_var {
         for ament_prefix_path in ament_prefix_paths.split(':') {
-            if let Some(clang_args) = generate_clang_include_arg(ament_prefix_path) {
-                builder = builder.clang_args(clang_args);
+            // Should not panic, since this was originally a valid UTF-8 String
+            builder = builder.clang_arg(format!("-I{}/include", ament_prefix_path));
+
+            // If this is RCL or rcutils, we need to add a special include
+            match ament_prefix_path.split('/').last() {
+                None => (),
+                Some(project)
+                    if project.eq("rcl")
+                        || project.eq("rcutils")
+                        || project.eq("rmw")
+                        || project.eq("rcl_yaml_param_parser")
+                        || project.eq("rosidl_runtime_c")
+                        || project.eq("rosidl_typesupport_interface") =>
+                {
+                    builder =
+                        builder.clang_arg(format!("-I{}/include/{}", ament_prefix_path, project))
+                }
+                Some(_) => (),
             }
+
             println!("cargo:rustc-link-search=native={}/lib", ament_prefix_path);
         }
     }
