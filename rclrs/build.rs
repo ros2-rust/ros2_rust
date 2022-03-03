92b44bdbdb723861706f73e012bae5dc166eb2d7
Jacob Hassold <jhassold@dcscorp.com>
2022-03-03 18:49:23 +0000

    Implementing GitHub suggestions Distro A, OPSEC #4584. You may have additional rights; please see https://rosmilitary.org/faq/?category=ros-2-license
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- rclrs/build.rs
+++ rclrs/build.rs
@@ -2,7 +2,7 @@ extern crate bindgen;
 
 use std::env;
 use std::fs::read_dir;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 fn main() {
     let mut builder = bindgen::Builder::default()
@@ -28,67 +28,64 @@ fn main() {
     let ament_prefix_var_name = "AMENT_PREFIX_PATH";
     let ament_prefix_var = env::var(ament_prefix_var_name);
 
+    // #############
     // # ALGORITHM #
-    // Search through ament index at AMENT_PREFIX_PATH../share/ament_index/resource_index/packages/ to find packages to include
-    // Packages will be located at (for each path in AMENT_PREFIX_PATHS):
-    // > AMENT_PREFIX_PATH/include/ (For the header files) (OLD STYLE)
-    //      -isystemAMENT_PREFIX_PATH/include
-    // > AMENT_PREFIX_PATH/include/project_name (NEW STYLE)
-    //      -isystemAMENT_PREFIX_PATH/include/project_name (if and only if it has a folder inside of it also named project_name)
-    //      -isystemAMENT_PREFIX_PATH/include/CycloneDDS (special case, match for this)
-    // -OR-
-    // > AMENT_PREFIX_PATH/lib  (For the link libraries)
+    // #############
+    //
+    // For each prefix in ${AMENT_PREFIX_PATH}:
+    // Search through ament index at ${prefix}/share/ament_index/resource_index/packages/ to find packages to include
+    // The include root will be located at either:
+    // - ${prefix}/include/ (old style)
+    // - ${prefix}/include/package_name (new style)
+    // - ${prefix}/include/CycloneDDS (special case, match for this)
+    // End of loop
+    // Compiled libraries are always at ${prefix}/lib
     //
     // See REP 122 for more details: https://www.ros.org/reps/rep-0122.html#filesystem-layout
 
     if let Ok(ament_prefix_paths) = ament_prefix_var {
-        for ament_prefix_path in ament_prefix_paths.split(':').filter_map(|p| {
-            let path = Path::new(p).to_path_buf();
-            if path.is_dir() {
-                Some(path)
-            } else {
-                None
-            }
-        }) {
+        for ament_prefix_path in ament_prefix_paths.split(':').map(PathBuf::from) {
             // Locate the ament index
-            let mut ament_index = ament_prefix_path.clone();
-            ament_index.push("share/ament_index/resource_index/packages");
+            let ament_index = ament_prefix_path.join("share/ament_index/resource_index/packages");
             if !ament_index.is_dir() {
                 continue;
             }
 
+            // Base directory all headers will be
+            let include_dir = ament_prefix_path.join("include");
+
             // Including the old-style packages
-            // todo<jhassold>: Remove this when old-style installation paths are fully removed
-            builder = builder.clang_arg(format!(
-                "-isystem{}/include",
-                ament_prefix_path.to_string_lossy()
-            ));
+            // todo(jhdcs): Remove this when old-style installation paths are fully removed
+            builder = builder.clang_arg(format!("-isystem{}", include_dir.to_string_lossy()));
 
-            // Search for and include new-style-converted packages
-            for package_index_path in read_dir(&ament_index)
-                .unwrap()
-                .filter_map(|p| p.ok().map(|p| p.path()))
-                .filter(|p| p.file_name().is_some())
-            {
-                let package = package_index_path.file_name().unwrap().to_string_lossy();
+            // Search for and include new-style-converted package paths
+            for dir_entry in read_dir(&ament_index).unwrap().filter_map(|p| p.ok()) {
+                let package = dir_entry.file_name().to_string_lossy().to_string();
+                let project_include_dir = include_dir.join(&package);
 
-                let mut include_dir = ament_prefix_path.clone();
-                include_dir.push(format!("include/{}", package.to_string()));
+                if project_include_dir.is_dir() {
+                    // If the project_include_dir has another directory within it with the same name as the project
+                    // then this is a new-style installation path
+                    // todo(jhdcs): remove this check when no more packages use the old style
+                    let new_style_include_dir = project_include_dir.join(&package);
 
-                if include_dir.is_dir() {
-                    let mut new_style_include_dir = include_dir.clone();
-                    new_style_include_dir.push(package.to_string());
+                    // CycloneDDS is a special case - it needs to be included as if it were a new-style path, but
+                    // doesn't actually have a secondary folder within it called "CycloneDDS"
+                    // todo(jhdcs): if this changes in future, remove this check
                     if package == "CycloneDDS" || new_style_include_dir.is_dir() {
-                        builder =
-                            builder.clang_arg(format!("-isystem{}", include_dir.to_string_lossy()));
+                        builder = builder.clang_arg(format!(
+                            "-isystem{}",
+                            project_include_dir.to_string_lossy()
+                        ));
                     }
                 }
             }
 
             // Link the native libraries
+            let library_path = ament_prefix_path.join("lib");
             println!(
-                "cargo:rustc-link-search=native={}/lib",
-                ament_prefix_path.to_string_lossy()
+                "cargo:rustc-link-search=native={}",
+                library_path.to_string_lossy()
             );
         }
     }
