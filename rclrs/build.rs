d6c70469b9d5b0c87841c82ec7ba8c600f4ac9ad
Jacob Hassold <jhassold@dcscorp.com>
2022-03-03 12:46:32 +0000

    Complete re-write. Works for both merge and symlink installs Distro A, OPSEC #4584. You may have additional rights; please see https://rosmilitary.org/faq/?category=ros-2-license
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- rclrs/build.rs
+++ rclrs/build.rs
@@ -1,7 +1,8 @@
 extern crate bindgen;
 
 use std::env;
-use std::path::PathBuf;
+use std::fs::read_dir;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let mut builder = bindgen::Builder::default()
@@ -24,32 +25,76 @@ fn main() {
             non_exhaustive: false,
         });
 
+    for (k, v) in env::vars() {
+        println!("{}={}", k, v)
+    }
+    println!("\n");
+
     let ament_prefix_var_name = "AMENT_PREFIX_PATH";
     let ament_prefix_var = env::var(ament_prefix_var_name);
 
-    if let Ok(ament_prefix_paths) = ament_prefix_var {
-        for ament_prefix_path in ament_prefix_paths.split(':') {
-            // Should not panic, since this was originally a valid UTF-8 String
-            builder = builder.clang_arg(format!("-I{}/include", ament_prefix_path));
+    // # ALGORITHM #
+    // Search through ament index at AMENT_PREFIX_PATH../share/ament_index/resource_index/packages/ to find packages to include
+    // Packages will be located at (for each path in AMENT_PREFIX_PATHS):
+    // > AMENT_PREFIX_PATH/include/ (For the header files) (OLD STYLE)
+    //      -isystemAMENT_PREFIX_PATH/include
+    // > AMENT_PREFIX_PATH/include/project_name (NEW STYLE)
+    //      -isystemAMENT_PREFIX_PATH/include/project_name (if and only if it has a folder inside of it also named project_name)
+    //      -isystemAMENT_PREFIX_PATH/include/CycloneDDS (special case, match for this)
+    // -OR-
+    // > AMENT_PREFIX_PATH/lib  (For the link libraries)
+    //
+    // See REP 122 for more details: https://www.ros.org/reps/rep-0122.html#filesystem-layout
 
-            // If this is RCL or rcutils, we need to add a special include
-            match ament_prefix_path.split('/').last() {
-                None => (),
-                Some(project)
-                    if project.eq("rcl")
-                        || project.eq("rcutils")
-                        || project.eq("rmw")
-                        || project.eq("rcl_yaml_param_parser")
-                        || project.eq("rosidl_runtime_c")
-                        || project.eq("rosidl_typesupport_interface") =>
-                {
-                    builder =
-                        builder.clang_arg(format!("-I{}/include/{}", ament_prefix_path, project))
-                }
-                Some(_) => (),
+    if let Ok(ament_prefix_paths) = ament_prefix_var {
+        for ament_prefix_path in ament_prefix_paths.split(':').filter_map(|p| {
+            let path = Path::new(p).to_path_buf();
+            if path.is_dir() {
+                Some(path)
+            } else {
+                None
+            }
+        }) {
+            // Locate the ament index
+            let mut ament_index = ament_prefix_path.clone();
+            ament_index.push("share/ament_index/resource_index/packages");
+            if !ament_index.is_dir() {
+                continue;
             }
 
-            println!("cargo:rustc-link-search=native={}/lib", ament_prefix_path);
+            // Including the old-style packages
+            // todo<jhassold>: Remove this when old-style installation paths are fully removed
+            builder = builder.clang_arg(format!(
+                "-isystem{}/include",
+                ament_prefix_path.to_string_lossy()
+            ));
+
+            // Search for and include new-style-converted packages
+            for package_index_path in read_dir(&ament_index)
+                .unwrap()
+                .filter_map(|p| p.ok().map(|p| p.path()))
+                .filter(|p| p.file_name().is_some())
+            {
+                let package = package_index_path.file_name().unwrap().to_string_lossy();
+
+                let mut include_dir = ament_prefix_path.clone();
+                include_dir.push(format!("include/{}", package.to_string()));
+
+                if include_dir.is_dir() {
+                    let mut new_style_include_dir = include_dir.clone();
+                    new_style_include_dir.push(package.to_string());
+                    if package == "CycloneDDS" || new_style_include_dir.is_dir() {
+                        builder =
+                            builder.clang_arg(format!("-isystem{}", include_dir.to_string_lossy()));
+                    }
+                }
+            }
+
+            // Link the native libraries
+            println!(
+                "cargo:rustc-link-search=native={}/lib",
+                ament_prefix_path.to_string_lossy()
+            );
         }
     }
 
