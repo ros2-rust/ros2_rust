53cc332de5da60065ac2f0e5d85c7c08c5f1eb33
Jacob Hassold <jhassold@dcscorp.com>
2022-03-04 13:10:52 +0000

    Use `.display` method, removed one todo, made todo clearer to see Distro A, OPSEC #4584. You may have additional rights; please see https://rosmilitary.org/faq/?category=ros-2-license
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- rclrs/build.rs
+++ rclrs/build.rs
@@ -2,7 +2,7 @@ extern crate bindgen;
 
 use std::env;
 use std::fs::read_dir;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let mut builder = bindgen::Builder::default()
@@ -36,7 +36,7 @@ fn main() {
     // Search through ament index at ${prefix}/share/ament_index/resource_index/packages/ to find packages to include
     // The include root will be located at either:
     // - ${prefix}/include/ (old style)
-    // - ${prefix}/include/package_name (new style)
+    // - ${prefix}/include/${package_name} (new style)
     // - ${prefix}/include/CycloneDDS (special case, match for this)
     // End of loop
     // Compiled libraries are always at ${prefix}/lib
@@ -44,49 +44,40 @@ fn main() {
     // See REP 122 for more details: https://www.ros.org/reps/rep-0122.html#filesystem-layout
 
     if let Ok(ament_prefix_paths) = ament_prefix_var {
-        for ament_prefix_path in ament_prefix_paths.split(':').map(PathBuf::from) {
+        for ament_prefix_path in ament_prefix_paths.split(':').map(Path::new) {
             // Locate the ament index
             let ament_index = ament_prefix_path.join("share/ament_index/resource_index/packages");
             if !ament_index.is_dir() {
                 continue;
             }
 
-            // Base directory all headers will be
+            // Old-style include directory
             let include_dir = ament_prefix_path.join("include");
 
             // Including the old-style packages
-            // todo(jhdcs): Remove this when old-style installation paths are fully removed
-            builder = builder.clang_arg(format!("-isystem{}", include_dir.to_string_lossy()));
+            builder = builder.clang_arg(format!("-isystem{}", include_dir.display()));
 
             // Search for and include new-style-converted package paths
             for dir_entry in read_dir(&ament_index).unwrap().filter_map(|p| p.ok()) {
-                let package = dir_entry.file_name().to_string_lossy().to_string();
-                let project_include_dir = include_dir.join(&package);
+                let package = dir_entry.file_name();
+                let package_include_dir = include_dir.join(&package);
 
-                if project_include_dir.is_dir() {
-                    // If the project_include_dir has another directory within it with the same name as the project
-                    // then this is a new-style installation path
-                    // todo(jhdcs): remove this check when no more packages use the old style
-                    let new_style_include_dir = project_include_dir.join(&package);
+                if package_include_dir.is_dir() {
+                    let new_style_include_dir = package_include_dir.join(&package);
 
                     // CycloneDDS is a special case - it needs to be included as if it were a new-style path, but
                     // doesn't actually have a secondary folder within it called "CycloneDDS"
-                    // todo(jhdcs): if this changes in future, remove this check
+                    // TODO(jhdcs): if this changes in future, remove this check
                     if package == "CycloneDDS" || new_style_include_dir.is_dir() {
-                        builder = builder.clang_arg(format!(
-                            "-isystem{}",
-                            project_include_dir.to_string_lossy()
-                        ));
+                        builder =
+                            builder.clang_arg(format!("-isystem{}", package_include_dir.display()));
                     }
                 }
             }
 
             // Link the native libraries
             let library_path = ament_prefix_path.join("lib");
-            println!(
-                "cargo:rustc-link-search=native={}",
-                library_path.to_string_lossy()
-            );
+            println!("cargo:rustc-link-search=native={}", library_path.display());
         }
     }
 
