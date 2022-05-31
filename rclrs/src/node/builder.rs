85e7fea258891b4aa616b3f7112218a14bfc7998
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-05-31 11:42:37 +0000

    Add methods related to node options to NodeBuilder

--- rclrs/src/node/builder.rs
+++ rclrs/src/node/builder.rs
@@ -13,6 +13,10 @@ use std::sync::Arc;
 ///
 /// The default values for optional fields are:
 /// - `namespace: "/"`
+/// - `domain_id: ROS_DOMAIN_ID`
+/// - `use_global_arguments: true`
+/// - `arguments: []`
+/// - `enable_rosout`: true`
 ///
 /// # Example
 /// ```
@@ -40,6 +44,10 @@ pub struct NodeBuilder {
     context: Arc<Mutex<rcl_context_t>>,
     name: String,
     namespace: String,
+    domain_id: usize,
+    use_global_arguments: bool,
+    arguments: Vec<String>,
+    enable_rosout: bool,
 }
 
 impl NodeBuilder {
@@ -80,10 +88,18 @@ impl NodeBuilder {
     /// [2]: https://docs.ros2.org/latest/api/rmw/validate__node__name_8h.html#a5690a285aed9735f89ef11950b6e39e3
     /// [3]: NodeBuilder::build
     pub fn new(context: &Context, name: &str) -> NodeBuilder {
+        let mut domain_id = 0;
+        let ret = unsafe { rcl_get_default_domain_id(&mut domain_id) };
+        debug_assert_eq!(ret, 0);
+
         NodeBuilder {
             context: context.handle.clone(),
             name: name.to_string(),
             namespace: "/".to_string(),
+            domain_id,
+            use_global_arguments: true,
+            arguments: vec![],
+            enable_rosout: true,
         }
     }
 
@@ -142,6 +158,102 @@ impl NodeBuilder {
         self
     }
 
+    /// Sets node specific domain id
+    ///
+    /// domain id is used in DDS to calculate UDP port that will be used for discovery and communication.
+    /// [This article][1] describes how to calculate UDP port number from domain id.
+    /// From formula in above article, available domain id is between 0 to 232.
+    /// To skip about background described above, you can safely choose between 0 to 101.
+    ///
+    /// For detailed descriptions, see [here][2]
+    ///
+    /// # Example
+    /// ```
+    /// # use rclrs::{Context, Node, NodeBuilder, RclrsError};
+    /// let context = Context::new([])?;
+    /// let node = context
+    ///   .create_node_builder("my_node")
+    ///   .domain_id(42)
+    ///   .build()?;
+    /// assert_eq!(node.domain_id(), 42);
+    /// # Ok::<(), RclrsError>(())
+    /// ```
+    ///
+    /// [1]: https://community.rti.com/content/forum-topic/statically-configure-firewall-let-omg-dds-traffic-through
+    /// [2]: https://docs.ros.org/en/foxy/Concepts/About-Domain-ID.html
+    pub fn domain_id(mut self, domain_id: usize) -> Self {
+        self.domain_id = domain_id;
+        self
+    }
+
+    /// Sets flag for using global arguments.
+    /// If false then only use arguments passed to this builder,
+    /// otherwise use global arguments also.
+    ///
+    /// # Example
+    /// ```
+    /// # use rclrs::{Context, Node, NodeBuilder, RclrsError};
+    /// let args = ["--ros-args", "--remap", "my_node:__node:=your_node"]
+    ///   .iter()
+    ///   .map(|s| s.to_string())
+    ///   .collect::<Vec<_>>();
+    /// let context = Context::new(args)?;
+    /// // Use global arguments
+    /// let node = context
+    ///   .create_node_builder("my_node")
+    ///   .use_global_arguments(true)
+    ///   .build()?;
+    /// assert_eq!(node.name(), "your_node");
+    ///
+    /// // Not use global arguments
+    /// let node = context
+    ///   .create_node_builder("my_node")
+    ///   .use_global_arguments(false)
+    ///   .build()?;
+    /// assert_eq!(node.name(), "my_node");
+    /// # Ok::<(), RclrsError>(())
+    pub fn use_global_arguments(mut self, enable: bool) -> Self {
+        self.use_global_arguments = enable;
+        self
+    }
+
+    /// Sets node specific command line arguments.
+    ///
+    /// Command line arguments can mix ROS specific and not specific arguments.
+    /// ROS specific arguments are wrapped between `--ros-args` and `--`.
+    /// Note that `--ros-args` followed by only ROS specific arguments is valid pattern.
+    ///
+    /// below arguments examples are valid:
+    ///   --ros-args -r from:=to -- user_arg0 user_arg1
+    ///   --ros-args -p name:=value
+    ///    
+    /// # Example
+    /// ```
+    /// # use rclrs::{Context, Node, NodeBuilder, RclrsError};
+    /// let context = Context::new([])?;
+    /// let node_args = ["--ros-args", "--remap", "my_node:__node:=your_node"]
+    ///   .iter()
+    ///   .map(|s| s.to_string())
+    ///   .collect::<Vec<_>>();
+    /// let node = context
+    ///   .create_node_builder("my_node")
+    ///   .arguments(node_args)
+    ///   .build()?;
+    /// assert_eq!(node.name(), "your_node");
+    /// # Ok::<(), RclrsError>(())
+    /// ```
+    pub fn arguments(mut self, arguments: impl IntoIterator<Item = String>) -> Self {
+        self.arguments = arguments.into_iter().collect();
+        self
+    }
+
+    /// Enables rosout.
+    /// If false, rosout logging is disabled.
+    pub fn enable_rosout(mut self, enable: bool) -> Self {
+        self.enable_rosout = enable;
+        self
+    }
+
     /// Builds the node instance.
     ///
     /// Node name and namespace validation is performed in this method.
@@ -149,7 +261,10 @@ impl NodeBuilder {
     /// For example usage, see the [`NodeBuilder`][1] docs.
     ///
     /// # Panics
-    /// When the node name or namespace contain null bytes.
+    /// If any of below conditions are filled, panic occurs
+    ///  - node name contains null byte
+    ///  - namespace contains null byte
+    ///  - any of node argument strings contain null byte.
     ///
     /// [1]: crate::NodeBuilder
     pub fn build(&self) -> Result<Node, RclrsError> {
@@ -159,11 +274,38 @@ impl NodeBuilder {
         // SAFETY: No preconditions for this function.
         let mut node_handle = unsafe { rcl_get_zero_initialized_node() };
 
+        // SAFETY: No preconditions for this function.
+        let mut node_options = unsafe { rcl_node_get_default_options() };
+
+        let cstr_args = self
+            .arguments
+            .iter()
+            .map(|s| CString::new(s.as_str()).unwrap())
+            .collect::<Vec<_>>();
+        let cstr_arg_ptrs = cstr_args.iter().map(|s| s.as_ptr()).collect::<Vec<_>>();
+        // SAFETY: No preconditions for this function.
+        let mut arguments = unsafe { rcl_get_zero_initialized_arguments() };
+        unsafe {
+            // SAFETY: No preconditions for this function.
+            rcl_parse_arguments(
+                cstr_arg_ptrs.len() as i32,
+                cstr_arg_ptrs.as_ptr(),
+                rcutils_get_default_allocator(),
+                &mut arguments,
+            )
+        }
+        .ok()?;
+
+        node_options.domain_id = self.domain_id;
+        node_options.arguments = arguments;
+        node_options.use_global_arguments = self.use_global_arguments;
+        node_options.enable_rosout = self.enable_rosout;
+        //SAFETY: No preconditions for this function.
+        node_options.allocator = unsafe { rcutils_get_default_allocator() };
+
         unsafe {
             // SAFETY: No preconditions for this function.
             let context_handle = &mut *self.context.lock();
-            // SAFETY: No preconditions for this function.
-            let node_options = rcl_node_get_default_options();
 
             // SAFETY: The node handle is zero-initialized as expected by this function.
             // The strings and node options are copied by this function, so we don't need
