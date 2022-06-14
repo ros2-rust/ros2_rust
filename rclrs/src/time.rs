efe7010370203438b899d24204e5dd966434018e
Deepesh Padala <deepesh.padala02@gmail.com>
2022-06-14 17:39:31 +0530

    added the Time struct

--- rclrs/src/time.rs
+++ rclrs/src/time.rs
@@ -1,8 +1,12 @@
+use crate::error::RclrsError;
+use crate::RclReturnCode;
 use crate::rcl_bindings::*;
 use std::os::raw::{c_void, c_uint};
+use std::time::Duration;
+
 
 /// Function to get the current steady time
-pub fn rcl_get_steady_time(current_time: *mut rcl_time_point_value_t) -> rcl_ret_t {
+fn rcl_get_steady_time(current_time: *mut rcl_time_point_value_t) -> rcl_ret_t {
     // SAFETY: No preconditions for this function
     unsafe {
         rcutils_steady_time_now(current_time)
@@ -10,7 +14,7 @@ pub fn rcl_get_steady_time(current_time: *mut rcl_time_point_value_t) -> rcl_ret
 }
 
 /// Function to return the current system time
-pub fn rcl_get_system_time(current_time: *mut rcl_time_point_value_t) -> rcl_ret_t {
+fn rcl_get_system_time(current_time: *mut rcl_time_point_value_t) -> rcl_ret_t {
     // SAFETY: No preconditions for this function
     unsafe {
         rcutils_system_time_now(current_time)
@@ -34,7 +38,7 @@ impl rcl_clock_t {
         }
     }*/
     /// Function to check validity of the clock
-    pub fn rcl_clock_valid(&self) -> bool {
+    fn rcl_clock_valid(&self) -> bool {
         let mut _flag: bool = false;
         match self.type_ {
             rcl_clock_type_t::RCL_CLOCK_UNINITIALIZED => _flag = false,
@@ -52,3 +56,28 @@ impl rcl_clock_t {
     }
 }
 
+pub struct Time {
+    seconds: i32,
+    nanoseconds: u32,
+    clock_type: rcl_clock_type_t,
+}
+
+impl Time {
+    /// Function to create a new instance of Time
+    pub fn new(seconds: i32, 
+    nanoseconds: u32, 
+    clock_type: rcl_clock_type_t) -> Result<Self, RclrsError> {
+        if seconds < 0 {
+            return Err(RclrsError::RclError{
+                code: RclReturnCode::InvalidArgument,
+                msg: None
+            });
+        }
+
+        Ok(Self {
+            seconds,
+            nanoseconds,
+            clock_type,
+        })
+    }
+}
