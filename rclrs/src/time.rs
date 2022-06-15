9c29dee4dbf63d2e65035cab6794b1bbbd993b3e
Deepesh Padala <deepesh.padala02@gmail.com>
2022-06-15 16:12:13 +0530

    changes made to time.rs and lib.rs

--- rclrs/src/time.rs
+++ rclrs/src/time.rs
@@ -1,81 +1,76 @@
 use crate::error::RclrsError;
 use crate::rcl_bindings::*;
-use crate::RclReturnCode;
-use std::os::raw::{c_uint, c_void};
-use std::time::Duration;
+//use crate::RclReturnCode;
+//use crate::duration;
+use parking_lot::Mutex;
+use std::time;
+
+
+/// Enum to provide different ways to construct the Time struct
+#[allow(missing_docs)]
+pub enum TimeFrom {
+    /// Create a Time object using seconds
+    Secs{s: u64},
+
+    /// Create a Time object using nanoseconds
+    NanoSecs{ns: u64},
+
+    /// Create a Time object using `std::time::Duration`
+    Duration{d: time::Duration},
+}
 
 /// The Time struct
 pub struct Time {
-    seconds: i32,
-    nanoseconds: u32,
-    clock_type: rcl_clock_type_t,
+    rcl_time_: Mutex<rcl_time_point_t>,
+    // wrapped in mutex to allow the Clock struct to access it
 }
 
-impl Time {
-    /// Function to create a new instance of Time
-    pub fn new(
-        seconds: i32,
-        nanoseconds: u32,
-        clock_type: rcl_clock_type_t,
-    ) -> Result<Self, RclrsError> {
-        if seconds < 0 {
-            return Err(RclrsError::RclError {
-                code: RclReturnCode::InvalidArgument,
-                msg: None,
-            });
+impl Clone for Time {
+    fn clone(&self) -> Self {
+        let lock = self.rcl_time_.lock();
+        Self {
+            rcl_time_: Mutex::new(rcl_time_point_t {
+                nanoseconds: (*lock).nanoseconds,
+                clock_type: (*lock).clock_type,
+            })
         }
-
-        Ok(Self {
-            seconds,
-            nanoseconds,
-            clock_type,
-        })
     }
 }
 
-/// Function to get the current steady time
-fn rcl_get_steady_time(current_time: *mut rcl_time_point_value_t) -> rcl_ret_t {
-    // SAFETY: No preconditions for this function
-    unsafe { rcutils_steady_time_now(current_time) }
-}
-
-/// Function to return the current system time
-fn rcl_get_system_time(current_time: *mut rcl_time_point_value_t) -> rcl_ret_t {
-    // SAFETY: No preconditions for this function
-    unsafe { rcutils_system_time_now(current_time) }
-}
-
-impl rcl_clock_t {
-    /*
-    /// Function to create a new rcl_clock_t instance
-    pub fn new(type_: rcl_clock_type_t, allocator: rcl_allocator_t) -> Self {
-        unsafe {
-        let empty_data = c_void::null;
-        Self {
-            type_: type_,
-            jump_callbacks: None,
-            num_jump_callbacks: 0 as usize,
-            get_now: None,
-            data: empty_data,
-            allocator: allocator,
-        }
-        }
-    }*/
-    /// Function to check validity of the clock
-    fn rcl_clock_valid(&self) -> bool {
-        let mut _flag: bool = false;
-        match self.type_ {
-            rcl_clock_type_t::RCL_CLOCK_UNINITIALIZED => _flag = false,
-            _ => _flag = true,
-        }
-
-        if !_flag {
-            match self.get_now {
-                Option::None => false,
-                _ => true,
-            }
-        } else {
-            _flag
+#[allow(dead_code)]
+impl Time {
+    /// Function to create a new instance of Time
+    ///
+    /// # Example
+    ///
+    /// ## Create a Time object using seconds
+    /// `Time::new(TimeFrom::Secs{s: <seconds>})`
+    ///
+    /// ## Create a Time object using nanoseconds
+    /// `Time::new(TimeFrom::NanoSecs{ns: <nanoseconds>})`
+    ///
+    /// ## Create a Time object using `std::time::Duration`
+    /// `Time::new(TimeFrom::Duration{d: std::time::Duration...})`
+    pub fn new(arg: TimeFrom, clock_type: rcl_clock_type_t) -> Result<Self, RclrsError> {
+        match arg {
+            TimeFrom::Secs{s} => Ok(Self {
+                rcl_time_: Mutex::new(rcl_time_point_t {
+                    nanoseconds: time::Duration::from_secs(s).as_nanos() as rcl_time_point_value_t,
+                    clock_type,
+                })
+            }),
+            TimeFrom::NanoSecs{ns} => Ok(Self {
+                rcl_time_: Mutex::new(rcl_time_point_t {
+                    nanoseconds: ns as rcl_time_point_value_t,
+                    clock_type,
+                }),
+            }),
+            TimeFrom::Duration{d} => Ok(Self {
+                rcl_time_: Mutex::new(rcl_time_point_t {
+                    nanoseconds: d.as_nanos() as rcl_time_point_value_t,
+                    clock_type,
+                }),
+            }),
         }
     }
 }
