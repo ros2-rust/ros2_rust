49fe3ed7ac413b6fe5fb869b3863680648c2e868
Deepesh Padala <deepesh.padala02@gmail.com>
2022-06-15 17:25:17 +0530

    complete time.rs

--- rclrs/src/time.rs
+++ rclrs/src/time.rs
@@ -3,20 +3,20 @@ use crate::rcl_bindings::*;
 //use crate::RclReturnCode;
 use crate::duration::Duration;
 use parking_lot::Mutex;
-use std::time;
 use std::ops::{Add, Sub};
+use std::time;
 
 /// Enum to provide different ways to construct the Time struct
 #[allow(missing_docs)]
 pub enum TimeFrom {
     /// Create a Time object using seconds
-    Secs{s: u64},
+    Secs { s: u64 },
 
     /// Create a Time object using nanoseconds
-    NanoSecs{ns: u64},
+    NanoSecs { ns: u64 },
 
     /// Create a Time object using `std::time::Duration`
-    Duration{d: time::Duration},
+    Duration { d: time::Duration },
 }
 
 /// The Time struct
@@ -32,13 +32,16 @@ impl Add<Duration> for Time {
         let lock = self.rcl_time_.lock();
         let sum = (*lock).nanoseconds + (rhs.nanoseconds() as rcl_time_point_value_t);
         if (sum as u128) > (rcl_time_point_value_t::MAX as u128) {
-            panic!("Addition causes an overlflow for {}", std::any::type_name::<rcl_time_point_value_t>())
+            panic!(
+                "Addition causes an overlflow for {}",
+                std::any::type_name::<rcl_time_point_value_t>()
+            )
         }
         Self {
             rcl_time_: Mutex::new(rcl_time_point_t {
                 nanoseconds: sum,
                 clock_type: (*lock).clock_type,
-            })
+            }),
         }
     }
 }
@@ -54,20 +57,34 @@ impl Sub for Time {
             panic!("Can not subtract times with different time sources");
         } else if diff < 0 {
             panic!("Time subtraction leads to negative time");
-        } else {
-            Self{
-                rcl_time_: Mutex::new(rcl_time_point_t {
-                    nanoseconds: diff,
-                    clock_type: lock.clock_type,
-                })
-            }
         }
-
-
+        Self {
+            rcl_time_: Mutex::new(rcl_time_point_t {
+                nanoseconds: diff,
+                clock_type: lock.clock_type,
+            }),
+        }
     }
 }
 
-//impl Sub<Duration>
+impl Sub<Duration> for Time {
+    type Output = Self;
+
+    fn sub(self, rhs: Duration) -> Self {
+        let lock = self.rcl_time_.lock();
+        let diff = lock.nanoseconds - (rhs.nanoseconds() as rcl_time_point_value_t);
+        if diff < 0 {
+            panic!("Subtraction leads to negative time");
+        }
+
+        Self {
+            rcl_time_: Mutex::new(rcl_time_point_t {
+                nanoseconds: diff,
+                clock_type: lock.clock_type,
+            }),
+        }
+    }
+}
 
 impl Clone for Time {
     fn clone(&self) -> Self {
@@ -76,11 +93,10 @@ impl Clone for Time {
             rcl_time_: Mutex::new(rcl_time_point_t {
                 nanoseconds: lock.nanoseconds,
                 clock_type: lock.clock_type,
-            })
+            }),
         }
     }
 }
-
 
 #[allow(dead_code)]
 impl Time {
@@ -98,23 +114,48 @@ impl Time {
     /// `Time::new(TimeFrom::Duration{d: std::time::Duration...})`
     pub fn new(arg: TimeFrom, clock_type: rcl_clock_type_t) -> Result<Self, RclrsError> {
         match arg {
-            TimeFrom::Secs{s} => Ok(Self {
+            TimeFrom::Secs { s } => Ok(Self {
                 rcl_time_: Mutex::new(rcl_time_point_t {
                     nanoseconds: time::Duration::from_secs(s).as_nanos() as rcl_time_point_value_t,
                     clock_type,
-                })
+                }),
             }),
-            TimeFrom::NanoSecs{ns} => Ok(Self {
+            TimeFrom::NanoSecs { ns } => Ok(Self {
                 rcl_time_: Mutex::new(rcl_time_point_t {
                     nanoseconds: ns as rcl_time_point_value_t,
                     clock_type,
                 }),
             }),
-            TimeFrom::Duration{d} => Ok(Self {
+            TimeFrom::Duration { d } => Ok(Self {
                 rcl_time_: Mutex::new(rcl_time_point_t {
                     nanoseconds: d.as_nanos() as rcl_time_point_value_t,
                     clock_type,
                 }),
+            }),
+        }
+    }
+
+    /// Function to return time in nanoseconds
+    fn nanoseconds(&self) -> rcl_time_point_value_t {
+        (*self.rcl_time_.lock()).nanoseconds
+    }
+
+    /// Function to return time in seconds
+    fn seconds(&self) -> u64 {
+        time::Duration::from_nanos((*self.rcl_time_.lock()).nanoseconds.try_into().unwrap())
+            .as_secs()
+    }
+
+    /// Function to return clock type
+    fn get_clock_type(&self) -> rcl_clock_type_t {
+        self.rcl_time_.lock().clock_type
+    }
+
+    fn max(&self) -> Self {
+        Self {
+            rcl_time_: Mutex::new(rcl_time_point_t {
+                nanoseconds: rcl_time_point_value_t::MAX,
+                clock_type: self.rcl_time_.lock().clock_type,
             }),
         }
     }
