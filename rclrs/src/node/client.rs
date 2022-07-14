eda4dad9e5d711bd83806c4761f3ae349faf362a
nnmm <nnmmgit@gmail.com>
2022-07-14 14:59:38 +0200

    Always add subscription/client/service to the node's list of waitables

--- rclrs/src/node/client.rs
+++ rclrs/src/node/client.rs
@@ -78,7 +78,7 @@ where
     T: rosidl_runtime_rs::Service,
 {
     /// Creates a new client.
-    pub fn new(node: &Node, topic: &str) -> Result<Self, RclrsError>
+    pub fn new(node: &mut Node, topic: &str) -> Result<Arc<Self>, RclrsError>
     where
         T: rosidl_runtime_rs::Service,
     {
@@ -114,14 +114,15 @@ where
             rcl_client_mtx: Mutex::new(rcl_client),
             rcl_node_mtx: node.rcl_node_mtx.clone(),
         });
-
-        Ok(Self {
+        let client = Arc::new(Self {
             handle,
             requests: Mutex::new(HashMap::new()),
             futures: Arc::new(Mutex::new(
                 HashMap::<RequestId, oneshot::Sender<T::Response>>::new(),
             )),
-        })
+        });
+        node.clients.push(Arc::downgrade(&client) as _);
+        Ok(client)
     }
 
     /// Sends a request with a callback to be called with the response.
