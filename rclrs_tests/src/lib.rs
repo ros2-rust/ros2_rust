542d651f3145fd81e2adb89566c49ee52cf605e8
Nikolai Morin <nikolai.morin@apex.ai>
2022-11-12 21:57:02 +0100

    Enable dynamic_message_tests for galactic

--- rclrs_tests/src/lib.rs
+++ rclrs_tests/src/lib.rs
@@ -2,7 +2,7 @@
 
 mod client_service_tests;
 // Disabled in Foxy due to https://github.com/ros2/rosidl/issues/598
-#[cfg(all(not(ros_distro = "foxy"), not(ros_distro = "galactic")))]
+#[cfg(not(ros_distro = "foxy"))]
 mod dynamic_message_tests;
 mod graph_tests;
 mod pub_sub_tests;
