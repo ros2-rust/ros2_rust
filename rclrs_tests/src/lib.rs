65c340f42546359cbaff47d7218b928486869de1
Nikolai Morin <nikolai.morin@apex.ai>
2022-11-12 21:57:12 +0100

    Revert "Enable dynamic_message_tests for galactic"
    This reverts commit 542d651f3145fd81e2adb89566c49ee52cf605e8.

--- rclrs_tests/src/lib.rs
+++ rclrs_tests/src/lib.rs
@@ -2,7 +2,7 @@
 
 mod client_service_tests;
 // Disabled in Foxy due to https://github.com/ros2/rosidl/issues/598
-#[cfg(not(ros_distro = "foxy"))]
+#[cfg(all(not(ros_distro = "foxy"), not(ros_distro = "galactic")))]
 mod dynamic_message_tests;
 mod graph_tests;
 mod pub_sub_tests;
