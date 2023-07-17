c075470c0c99f9e9092bd6554e93698fc176d30a
Esteve Fernandez <esteve@apache.org>
2023-07-17 13:55:08 +0200

    checkin

--- rclrs/src/node.rs
+++ rclrs/src/node.rs
@@ -13,9 +13,10 @@ pub use self::builder::*;
 pub use self::graph::*;
 use crate::rcl_bindings::*;
 use crate::{
-    ActionClient, Client, ClientBase, Clock, Context, GuardCondition, ParameterBuilder,
-    ParameterInterface, ParameterVariant, Parameters, Publisher, QoSProfile, RclrsError, Service,
-    ServiceBase, Subscription, SubscriptionBase, SubscriptionCallback, TimeSource, ToResult,
+    ActionClient, ActionServer, CancelResponse, Client, ClientBase, Clock, Context, GoalResponse,
+    GoalUUID, GuardCondition, ParameterInterface, ParameterVariant, Parameters, Publisher,
+    QoSProfile, RclrsError, ServerGoalHandle, Service, ServiceBase, Subscription, SubscriptionBase,
+    SubscriptionCallback, TimeSource, ToResult,
 };
 
 impl Drop for rcl_node_t {
@@ -195,7 +196,7 @@ impl Node {
         Ok(client)
     }
 
-    /// Creates a [`Client`][1].
+    /// Creates an [`ActionClient`][1].
     ///
     /// [1]: crate::ActionClient
     // TODO: make action client's lifetime depend on node's lifetime
@@ -206,13 +207,36 @@ impl Node {
     where
         T: rosidl_runtime_rs::Action,
     {
-        let client = Arc::new(ActionClient::<T>::new(
+        let action_client = Arc::new(ActionClient::<T>::new(
             Arc::clone(&self.rcl_node_mtx),
             topic,
         )?);
         // self.clients
         //     .push(Arc::downgrade(&client) as Weak<dyn ClientBase>);
-        Ok(client)
+        Ok(action_client)
+    }
+
+    /// Creates an [`ActionServer`][1].
+    ///
+    /// [1]: crate::ActionServer
+    // TODO: make action server's lifetime depend on node's lifetime
+    pub fn create_action_server<T>(
+        &mut self,
+        topic: &str,
+        handle_goal: fn(&crate::action::GoalUUID, Arc<T::Goal>) -> GoalResponse,
+        handle_cancel: fn(Arc<ServerGoalHandle<T>>) -> CancelResponse,
+        handle_accepted: fn(Arc<ServerGoalHandle<T>>),
+    ) -> Result<Arc<ActionServer<T>>, RclrsError>
+    where
+        T: rosidl_runtime_rs::Action,
+    {
+        let action_server = Arc::new(ActionServer::<T>::new(
+            Arc::clone(&self.rcl_node_mtx),
+            topic,
+        )?);
+        // self.servers
+        //     .push(Arc::downgrade(&server) as Weak<dyn ClientBase>);
+        Ok(action_server)
     }
 
     /// Creates a [`GuardCondition`][1] with no callback.
--- rclrs/src/rcl_wrapper.h
+++ rclrs/src/rcl_wrapper.h
@@ -1,5 +1,7 @@
 #include <rcl/graph.h>
 #include <rcl/rcl.h>
+#include <rcl_action/types.h>
+#include <rcl_action/goal_handle.h>
 #include <rcl_yaml_param_parser/parser.h>
 #include <rcutils/error_handling.h>
 #include <rmw/types.h>
@@ -7,3 +9,4 @@
 #include <rosidl_typesupport_introspection_c/message_introspection.h>
 
 const size_t rmw_gid_storage_size_constant = RMW_GID_STORAGE_SIZE;
+const size_t rcl_action_uuid_size_constant = UUID_SIZE;
