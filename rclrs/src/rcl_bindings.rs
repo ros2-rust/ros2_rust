e4b251b959eb10d388444e9ac5903ff5bcadc79d
carter <carterjschultz@gmail.com>
2023-11-07 18:41:18 -0700

    Extensions to get working

--- rclrs/src/rcl_bindings.rs
+++ rclrs/src/rcl_bindings.rs
@@ -130,18 +130,18 @@ cfg_if::cfg_if! {
             pub fn rcl_context_is_valid(context: *const rcl_context_t) -> bool;
         }
     } else {
-        #![allow(dead_code)]
-        #![allow(deref_nullptr)]
-        #![allow(non_upper_case_globals)]
-        #![allow(non_camel_case_types)]
-        #![allow(non_snake_case)]
+        #[allow(dead_code)]
+        #[allow(deref_nullptr)]
+        #[allow(non_upper_case_globals)]
+        #[allow(non_camel_case_types)]
+        #[allow(non_snake_case)]
         // Added to avoid clippy complaining about u128 types not being FFI safe
         // Caused by https://github.com/ros2/ros2/issues/1374 in iron and newer
         // See https://github.com/ros2-rust/ros2_rust/issues/320
-        #![allow(improper_ctypes)]
-        #![allow(improper_ctypes_definitions)]
-        #![allow(clippy::all)]
-        #![allow(missing_docs)]
+        #[allow(improper_ctypes)]
+        #[allow(improper_ctypes_definitions)]
+        #[allow(clippy::all)]
+        #[allow(missing_docs)]
         include!(concat!(env!("OUT_DIR"), "/rcl_bindings_generated.rs"));
 
         pub const RMW_GID_STORAGE_SIZE: usize = rmw_gid_storage_size_constant;
--- rosidl_runtime_rs/build.rs
+++ rosidl_runtime_rs/build.rs
@@ -15,10 +15,13 @@ fn get_env_var_or_abort(env_var: &'static str) -> String {
 }
 
 fn main() {
-    let ament_prefix_path_list = get_env_var_or_abort(AMENT_PREFIX_PATH);
-    for ament_prefix_path in ament_prefix_path_list.split(':') {
-        let library_path = Path::new(ament_prefix_path).join("lib");
-        println!("cargo:rustc-link-search=native={}", library_path.display());
+    #[cfg(not(feature = "generate_docs"))]
+    {
+        let ament_prefix_path_list = get_env_var_or_abort(AMENT_PREFIX_PATH);
+        for ament_prefix_path in ament_prefix_path_list.split(':') {
+            let library_path = Path::new(ament_prefix_path).join("lib");
+            println!("cargo:rustc-link-search=native={}", library_path.display());
+        }
     }
 
     // Invalidate the built crate whenever this script changes
