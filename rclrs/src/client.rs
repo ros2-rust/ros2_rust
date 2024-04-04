cbe8e0fe30669df9788a28958537fd9f9c23433a
GueLaKais <koroyeldores@gmail.com>
2024-04-04 23:59:54 +0200

    Refactoring of Dependency usage

--- rclrs/src/client.rs
+++ rclrs/src/client.rs
@@ -1,15 +1,20 @@
-use std::boxed::Box;
-use std::collections::HashMap;
-use std::ffi::CString;
-use std::sync::atomic::AtomicBool;
-use std::sync::{Arc, Mutex, MutexGuard};
+use std::{
+    boxed::Box,
+    collections::HashMap,
+    ffi::CString,
+    sync::{
+        atomic::AtomicBool,
+        {Arc, Mutex, MutexGuard}
+    }};
 
 use futures::channel::oneshot;
 use rosidl_runtime_rs::Message;
 
-use crate::error::{RclReturnCode, ToResult};
-use crate::MessageCow;
-use crate::{rcl_bindings::*, RclrsError};
+use crate::{
+    error::{RclReturnCode, ToResult},
+    MessageCow,
+    {rcl_bindings::*, RclrsError}
+};
 
 // SAFETY: The functions accessing this type, including drop(), shouldn't care about the thread
 // they are running in. Therefore, this type can be safely sent to another thread.
