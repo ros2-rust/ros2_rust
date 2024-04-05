b08a954487873d257ff025f9b4d9e6937d059d4d
GueLaKais <koroyeldores@gmail.com>
2024-04-05 10:02:15 +0200

    Dependency import refactoring

--- rclrs/src/parameter/value.rs
+++ rclrs/src/parameter/value.rs
@@ -1,7 +1,6 @@
 use std::{ffi::CStr,sync::Arc};
 
-use crate::rcl_bindings::*;
-use crate::{ParameterRange, ParameterRanges, ParameterValueError};
+use crate::{rcl_bindings::*,ParameterRange, ParameterRanges, ParameterValueError};
 
 /// A parameter value.
 ///
