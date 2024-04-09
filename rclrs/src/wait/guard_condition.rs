5bab99f2d040fbb6a1d1c73cee27d7aad6220186
GueLaKais <koroyeldores@gmail.com>
2024-04-09 11:53:18 +0200

    Update code formatting configuration through .rustfmt.toml
    This commit updates the code formatting configuration for this project.
    
    Refer to the project-specific `.rustfmt.toml` file (located alongside
    Cargo.toml) for details on the specific formatting changes made.

--- rclrs/src/wait/guard_condition.rs
+++ rclrs/src/wait/guard_condition.rs
@@ -1,16 +1,16 @@
 use std::sync::{atomic::AtomicBool, Arc, Mutex};
 
-use crate::rcl_bindings::*;
-use crate::{Context, ContextHandle, RclrsError, ToResult};
+use crate::{rcl_bindings::*, Context, ContextHandle, RclrsError, ToResult};
 
 /// A waitable entity used for waking up a wait set manually.
 ///
-/// If a wait set that is currently waiting on events should be interrupted from a separate thread, this can be done
-/// by adding an `Arc<GuardCondition>` to the wait set, and calling `trigger()` on the same `GuardCondition` while
-/// the wait set is waiting.
+/// If a wait set that is currently waiting on events should be interrupted from
+/// a separate thread, this can be done by adding an `Arc<GuardCondition>` to
+/// the wait set, and calling `trigger()` on the same `GuardCondition` while the
+/// wait set is waiting.
 ///
-/// The guard condition may be reused multiple times, but like other waitable entities, can not be used in
-/// multiple wait sets concurrently.
+/// The guard condition may be reused multiple times, but like other waitable
+/// entities, can not be used in multiple wait sets concurrently.
 ///
 /// # Example
 /// ```
@@ -48,13 +48,14 @@ pub struct GuardCondition {
     pub(crate) handle: GuardConditionHandle,
     /// An optional callback to call when this guard condition is triggered.
     callback: Option<Box<dyn Fn() + Send + Sync>>,
-    /// A flag to indicate if this guard condition has already been assigned to a wait set.
+    /// A flag to indicate if this guard condition has already been assigned to
+    /// a wait set.
     pub(crate) in_use_by_wait_set: Arc<AtomicBool>,
 }
 
-/// Manage the lifecycle of an `rcl_guard_condition_t`, including managing its dependency
-/// on `rcl_context_t` by ensuring that this dependency is [dropped after][1] the
-/// `rcl_guard_condition_t`.
+/// Manage the lifecycle of an `rcl_guard_condition_t`, including managing its
+/// dependency on `rcl_context_t` by ensuring that this dependency is [dropped
+/// after][1] the `rcl_guard_condition_t`.
 ///
 /// [1]: <https://doc.rust-lang.org/reference/destructors.html>
 pub(crate) struct GuardConditionHandle {
@@ -67,7 +68,8 @@ pub(crate) struct GuardConditionHandle {
 impl Drop for GuardCondition {
     fn drop(&mut self) {
         unsafe {
-            // SAFETY: No precondition for this function (besides passing in a valid guard condition)
+            // SAFETY: No precondition for this function (besides passing in a valid guard
+            // condition)
             rcl_guard_condition_fini(&mut *self.handle.rcl_guard_condition.lock().unwrap());
         }
     }
@@ -75,8 +77,9 @@ impl Drop for GuardCondition {
 
 impl PartialEq for GuardCondition {
     fn eq(&self, other: &Self) -> bool {
-        // Because GuardCondition controls the creation of the rcl_guard_condition, each unique GuardCondition should have a unique
-        // rcl_guard_condition. Thus comparing equality of this member should be enough.
+        // Because GuardCondition controls the creation of the rcl_guard_condition, each
+        // unique GuardCondition should have a unique rcl_guard_condition. Thus
+        // comparing equality of this member should be enough.
         std::ptr::eq(
             &self.handle.rcl_guard_condition.lock().unwrap().impl_,
             &other.handle.rcl_guard_condition.lock().unwrap().impl_,
@@ -86,7 +89,8 @@ impl PartialEq for GuardCondition {
 
 impl Eq for GuardCondition {}
 
-// SAFETY: rcl_guard_condition is the only member that doesn't implement Send, and it is designed to be accessed from other threads
+// SAFETY: rcl_guard_condition is the only member that doesn't implement Send,
+// and it is designed to be accessed from other threads
 unsafe impl Send for rcl_guard_condition_t {}
 
 impl GuardCondition {
@@ -106,8 +110,10 @@ impl GuardCondition {
         )
     }
 
-    /// Creates a new guard condition by providing the rcl_context_t and an optional callback.
-    /// Note this function enables calling `Node::create_guard_condition`[1] without providing the Context separately
+    /// Creates a new guard condition by providing the rcl_context_t and an
+    /// optional callback. Note this function enables calling
+    /// `Node::create_guard_condition`[1] without providing the Context
+    /// separately
     ///
     /// [1]: Node::create_guard_condition
     pub(crate) fn new_with_context_handle(
@@ -119,7 +125,8 @@ impl GuardCondition {
             let mut guard_condition = unsafe { rcl_get_zero_initialized_guard_condition() };
             let mut rcl_context = context_handle.rcl_context.lock().unwrap();
             unsafe {
-                // SAFETY: The context must be valid, and the guard condition must be zero-initialized
+                // SAFETY: The context must be valid, and the guard condition must be
+                // zero-initialized
                 rcl_guard_condition_init(
                     &mut guard_condition,
                     &mut *rcl_context,
@@ -140,7 +147,8 @@ impl GuardCondition {
         }
     }
 
-    /// Triggers this guard condition, activating the wait set, and calling the optionally assigned callback.
+    /// Triggers this guard condition, activating the wait set, and calling the
+    /// optionally assigned callback.
     pub fn trigger(&self) -> Result<(), RclrsError> {
         unsafe {
             // SAFETY: The rcl_guard_condition_t is valid.
