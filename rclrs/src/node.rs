55749182814d93d9bb11810bfca0dc8fdc190131
GueLaKais <koroyeldores@gmail.com>
2024-04-09 13:20:25 +0200

    Run rustfmt
    Does change the things described in the newly added `.rustfmt.toml`

--- rclrs/src/node.rs
+++ rclrs/src/node.rs
@@ -1,22 +1,22 @@
 mod builder;
 mod graph;
-use std::cmp::PartialEq;
-use std::ffi::CStr;
-use std::fmt;
-use std::os::raw::c_char;
-use std::sync::{Arc, Mutex, Weak};
-use std::vec::Vec;
+use std::{
+    cmp::PartialEq,
+    ffi::CStr,
+    fmt,
+    os::raw::c_char,
+    sync::{Arc, Mutex, Weak},
+    vec::Vec,
+};
 
 use rosidl_runtime_rs::Message;
 
-pub use self::builder::*;
-pub use self::graph::*;
-use crate::rcl_bindings::*;
+pub use self::{builder::*, graph::*};
 use crate::{
-    Client, ClientBase, Clock, Context, ContextHandle, GuardCondition, ParameterBuilder,
-    ParameterInterface, ParameterVariant, Parameters, Publisher, QoSProfile, RclrsError, Service,
-    ServiceBase, Subscription, SubscriptionBase, SubscriptionCallback, TimeSource,
-    ENTITY_LIFECYCLE_MUTEX,
+    rcl_bindings::*, Client, ClientBase, Clock, Context, ContextHandle, GuardCondition,
+    ParameterBuilder, ParameterInterface, ParameterVariant, Parameters, Publisher, QoSProfile,
+    RclrsError, Service, ServiceBase, Subscription, SubscriptionBase, SubscriptionCallback,
+    TimeSource, ENTITY_LIFECYCLE_MUTEX,
 };
 
 // SAFETY: The functions accessing this type, including drop(), shouldn't care about the thread
