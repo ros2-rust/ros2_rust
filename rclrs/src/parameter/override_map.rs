5cddd54a8df8da0f4623d5729442f982950c3195
Michael X. Grey <grey@openrobotics.org>
2024-10-03 15:42:10 +0800

    Wrap slice::from_raw_parts to be compatible with rcl
    Signed-off-by: Michael X. Grey <grey@openrobotics.org>

--- rclrs/src/parameter/override_map.rs
+++ rclrs/src/parameter/override_map.rs
@@ -49,9 +49,9 @@ impl RclParamsIter<'_> {
             }
         } else {
             let node_name_ptrs =
-                std::slice::from_raw_parts((*rcl_params).node_names, (*rcl_params).num_nodes);
+                rcl_from_raw_parts((*rcl_params).node_names, (*rcl_params).num_nodes);
             let rcl_node_params =
-                std::slice::from_raw_parts((*rcl_params).params, (*rcl_params).num_nodes);
+                rcl_from_raw_parts((*rcl_params).params, (*rcl_params).num_nodes);
             Self {
                 node_name_ptrs,
                 rcl_node_params,
@@ -83,8 +83,8 @@ impl<'a> RclNodeParamsIter<'a> {
     // sizes or dangling pointers.
     pub unsafe fn new(rcl_node_params: &'a rcl_node_params_t) -> Self {
         let param_name_ptrs =
-            std::slice::from_raw_parts(rcl_node_params.parameter_names, rcl_node_params.num_params);
-        let rcl_variants = std::slice::from_raw_parts(
+            rcl_from_raw_parts(rcl_node_params.parameter_names, rcl_node_params.num_params);
+        let rcl_variants = rcl_from_raw_parts(
             rcl_node_params.parameter_values,
             rcl_node_params.num_params,
         );
