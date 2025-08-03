faf29da01e8ab117f34c32dcb7dd20b49b5a92b1
Sam Privett <sam@privett.dev>
2025-08-03 11:13:07 -0700

    trigger ci

--- docker/install_colcon_plugins.sh
+++ docker/install_colcon_plugins.sh
@@ -4,7 +4,7 @@ if [ "$ROS_DISTRO" = "humble" ]; then
   pip install --upgrade pytest
   pip install \
     git+https://github.com/colcon/colcon-cargo.git \
-     git+https://github.com/colcon/colcon-ros-cargo.git
+    git+https://github.com/colcon/colcon-ros-cargo.git
 else
   pip install --break-system-packages pytest \
     git+https://github.com/colcon/colcon-cargo.git \
