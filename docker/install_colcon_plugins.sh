62abac36c60dd4af0ed505f35f3f6cda6ed4db09
Sam Privett <sam@privett.dev>
2025-08-03 10:09:51 -0700

    Install the latest colcon-cargo and colcon-ros-cargo

--- docker/install_colcon_plugins.sh
+++ docker/install_colcon_plugins.sh
@@ -2,7 +2,11 @@
 
 if [ "$ROS_DISTRO" = "humble" ]; then
   pip install --upgrade pytest
-  pip install colcon-ros-cargo
+  pip install \
+    git+https://github.com/colcon/colcon-cargo.git \
+    git+https://github.com/colcon/colcon-ros-cargo.git
 else
-  pip install --break-system-packages pytest colcon-ros-cargo
+  pip install --break-system-packages pytest \
+    git+https://github.com/colcon/colcon-cargo.git \
+    git+https://github.com/colcon/colcon-ros-cargo.git
 fi
